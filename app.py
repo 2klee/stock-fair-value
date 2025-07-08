a2419cccf8718e6e1cc7a612e10f5760b89c9be6
2klee <148945895+2klee@users.noreply.github.com>
2025-07-08 13:36:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ KRX_HEADERS = {
 def get_krx_merged_basic_info(date):
     kospi_url = "https://data-dbg.krx.co.kr/svc/apis/sto/stk/stk_isu_base_info.json"
     kosdaq_url = "https://data-dbg.krx.co.kr/svc/apis/sto/ksq/ksq_isu_base_info.json"
-    params = {"basDt": date}
+    params = {"basDd": date}
 
     try:
         kospi_data = requests.get(kospi_url, params=params, headers=KRX_HEADERS).json().get("OutBlock_1", [])
@@ -33,7 +33,7 @@ def get_krx_merged_basic_info(date):
 # 일별 시세 가져오기
 def get_krx_daily_trading_info(date):
     url = "https://data-dbg.krx.co.kr/svc/apis/sto/sto/stk_bydd_trd.json"
-    params = {"basDt": date}
+    params = {"basDd": date}
 
     try:
         data = requests.get(url, params=params, headers=KRX_HEADERS).json().get("OutBlock_1", [])
